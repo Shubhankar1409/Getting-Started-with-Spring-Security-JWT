bee7c2075564d919b359390d05fd6a9231a855a8
Shubhankar1409 <shubhankarpaul1409@gmail.com>
2025-03-26 00:58:30 +0530

    Getting started with spring security

---
+++ src/main/java/com/dailycodebuffer/security/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.dailycodebuffer.security.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dailycodebuffer.security.entity.User;
+import com.dailycodebuffer.security.repository.UserRepository;
+import com.dailycodebuffer.security.service.UserService;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    private final UserService userService;
+
+    public UserController(UserRepository userRepository, UserService userService) {
+        this.userRepository = userRepository;
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        //return userRepository.save(user);
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        return userService.verify(user);
+    }
+
+}
